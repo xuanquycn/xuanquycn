74cd10c0f7a40ce355131ed36017a71f0f5e73c6
quylx <xuanquycn@gmail.com>
2022-03-26 11:05:52 +0700

    abc

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, url_for, render_template, request, redirect
-from .models import db, Product
+from .models import db, Question, Answer
 from flask_migrate import Migrate
 
 def create_app():
@@ -9,7 +9,7 @@ def create_app():
     # app.config ["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///products.sqlite3'
     
     app.config.update(
-        SQLALCHEMY_DATABASE_URI = f"sqlite:///{app.instance_path}/products.db",
+        SQLALCHEMY_DATABASE_URI = f"sqlite:///{app.instance_path}/survey.db",
         SQLALCHEMY_TRACK_MODIFICATIONS = False
     )
     
@@ -18,13 +18,41 @@ def create_app():
     migrate.init_app(app, db)
 
     
-    @app.route("/")
-    def index():
-        p1 = Product(name="K5")
-        p2 = Product(name="K8")
-    
-        db.session.add(p1)
-        db.session.add(p2)
+    @app.route("/init-db")
+    def init():
+        list_questions = []
+        list_answer = []
+        list_questions.append(Question(id= 1, title="Thiết kế", type = "radio"))
+        list_questions.append(Question(id= 2, title="Ứng dụng đa dạng , tiện ích", type = "radio"))
+        list_questions.append(Question(id= 3, title="Giá cả", type = "radio"))
+        list_questions.append(Question(id= 4, title="Chất lượng sản phẩm tốt", type = "radio"))
+        list_questions.append(Question(id= 5, title="Màu sắc ưa thích", type = "checkbox"))
+        
+        list_answer.append(Answer(id = 1, content="Không hài lòng", question_id = 1))
+        list_answer.append(Answer(id = 2, content="Hài lòng", question_id = 1))
+        list_answer.append(Answer(id = 3, content="Rất hài lòng", question_id = 1))
+        
+        list_answer.append(Answer(id = 4, content="Không hài lòng", question_id = 2))
+        list_answer.append(Answer(id = 5, content="Hài lòng", question_id = 2))
+        list_answer.append(Answer(id = 6, content="Rất hài lòng", question_id = 2))
+        
+        list_answer.append(Answer(id = 7, content="Hợp lý", question_id = 3))
+        list_answer.append(Answer(id = 8, content="Đắt", question_id = 3))
+        list_answer.append(Answer(id = 9, content="Rẻ", question_id = 3))
+        
+        list_answer.append(Answer(id = 10, content="Kém", question_id = 4))
+        list_answer.append(Answer(id = 11, content="Tốt", question_id = 4))
+        list_answer.append(Answer(id = 12, content="Rất tốt", question_id = 4))
+        
+        list_answer.append(Answer(id = 13, content="Xanh", question_id = 5))
+        list_answer.append(Answer(id = 14, content="Trắng", question_id = 5))
+        list_answer.append(Answer(id = 15, content="Đen", question_id = 5))
+        list_answer.append(Answer(id = 16, content="Hồng", question_id = 5))
+        
+        for i in range(4):
+            db.session.add(list_questions[i])
+        for i in range(15):
+            db.session.add(list_answer[i])
         db.session.commit()
 
         return render_template("index.html", data={"name": "Lê Xuân Quý"})
@@ -34,34 +62,34 @@ def create_app():
         return "Đây là trang tin tức!"
     
     
-    @app.route("/product", methods = ['GET', "DELETE"])
-    def product():
-        products = Product.query.all()
-        if request.method == 'GET':
-             return render_template("product/list.html", products=products)
-        elif request.method == "DELETE":
-            Product.query.filter_by(id=id).delete()
-            db.session.commit()
+    # @app.route("/product", methods = ['GET', "DELETE"])
+    # def product():
+    #     products = Product.query.all()
+    #     if request.method == 'GET':
+    #          return render_template("product/list.html", products=products)
+    #     elif request.method == "DELETE":
+    #         Product.query.filter_by(id=id).delete()
+    #         db.session.commit()
     
-    @app.route("/news-detail/<int:id>")
-    @app.route("/product/<int:id>")
-    def detail(id):
-        p = Product.query.filter_by(id=id).first()
-        return render_template("product/detail.html", data = p)
+    # @app.route("/news-detail/<int:id>")
+    # @app.route("/product/<int:id>")
+    # def detail(id):
+    #     p = Product.query.filter_by(id=id).first()
+    #     return render_template("product/detail.html", data = p)
     
-    @app.route("/add")
-    def add():
-        return "Đây là trang tin tức!"
+    # @app.route("/add")
+    # def add():
+    #     return "Đây là trang tin tức!"
     
-    @app.route('/edit/<int:id>', methods = ['GET', 'POST'])
-    def edit(id):
-        p = Product.query.filter_by(id=id).first()
-        if request.method == 'GET':
-            return render_template("product/edit.html", data = p)
-        else:
-            p.name = request.form['name']
-            db.session.commit()
-            return redirect(url_for('product'))
+    # @app.route('/edit/<int:id>', methods = ['GET', 'POST'])
+    # def edit(id):
+    #     p = Product.query.filter_by(id=id).first()
+    #     if request.method == 'GET':
+    #         return render_template("product/edit.html", data = p)
+    #     else:
+    #         p.name = request.form['name']
+    #         db.session.commit()
+    #         return redirect(url_for('product'))
         
                 
     return app
--- app/models.py
+++ app/models.py
@@ -2,10 +2,37 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 db = SQLAlchemy()
-
-class Product(db.Model):
-    __tablename__ = 'product'
+    
+    
+class Question(db.Model):
+    __tablename__ = 'question'
     
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
-    created_date = db.Column(db.DateTime, default=datetime.today)
+    title = db.Column(db.String(250), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    
+class Answer(db.Model):
+    __tablename__ = 'answer'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, nullable=False)
+    content = db.Column(db.String(250), nullable=False)
+
+class CustomerAnswer(db.Model):
+    __tablename__ = 'customeranswer'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question_id = db.Column(db.Integer, nullable=False)
+    answer_id = db.Column(db.Integer, nullable=False)
+    customer_id = db.Column(db.Integer, nullable=False)
+    
+class Customer(db.Model):
+    __tablename__ = 'customer'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(250), nullable=False)
+    eamill = db.Column(db.String(250), nullable=False)
+    job = db.Column(db.String(250), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    income = db.Column(db.Integer, nullable=False)
+    
